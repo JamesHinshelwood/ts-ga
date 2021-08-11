f0e97b632c6574a396345ff94e98bbc407538d71
James Hinshelwood <james.hinshelwood@bigpayme.com>
2021-08-11 21:46:08 +0100

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import * as core from '@actions/core'
+//import * as github from '@actions/github'
 import {wait} from './wait'
+import * as fs from 'fs'
 
 async function run(): Promise<void> {
   try {
+    const token = core.getInput('token')
+    core.debug(`Token: ${token}`)
+    //const octokit = github.getOctokit(token)
+
+    const files = await fs.promises.readdir('test/migration')
+
+    const re = /V([1-9][0-9]*)__[a-zA-Z_]+.sql/;
+    const versions = files.map(file => file.match(re)![1]);
+    if (versions.length != (new Set(versions)).size) {
+      core.setFailed(`Duplicate migration versions detected`);
+      return;
+    }
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
