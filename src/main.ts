6f22303fab6934cf5a0350833c781687f89ffe8d
James Hinshelwood <james.hinshelwood@bigpayme.com>
2021-08-11 21:46:08 +0100

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {wait} from './wait'
+import * as fs from 'fs'
+import { promisify } from 'util';
 
 async function run(): Promise<void> {
   try {
+    const token = core.getInput("token");
+    const octokit = github.getOctokit(token);
+
+    const files = await fs.promises.readdir("ppc/user-service/src/main/resources/db/migration");
+    
+    files.forEach(file => {
+      core.debug(`File found: ${file}`);
+    });
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
